12ea12f0446163bb22b7965cb38c6b197fa0cf94
nour <nourbenyoussef73@gmail.com>
2023-12-20 22:49:42 +0100

    télécharger un fichier depuis le client dans le serveur

--- puttftp.c
+++ puttftp.c
@@ -17,24 +17,28 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    // Configuration des paramètres pour obtenir les informations d'adresse
     struct addrinfo hints, *res;
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_DGRAM;
     hints.ai_protocol = IPPROTO_UDP;
 
+    // Obtention des informations d'adresse pour le serveur
     int a = getaddrinfo(argv[1], "1069", &hints, &res);
     if (a != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(a));
         return 1;
     }
 
+    // Création du socket
     int sck = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
     if (sck == -1) {
         perror("socket");
         return 1;
     }
 
+    // Construction du paquet TFTP de demande d'écriture (WRQ)
     char buffer[MAX_BUFFER_SIZE];
     buffer[0] = 0;
     buffer[1] = 2; // Opcode 2 pour la demande d'écriture
@@ -44,6 +48,7 @@ int main(int argc, char *argv[]) {
 
     int size = 2 + strlen(filename) + 1 + strlen("octet") + 1;
 
+    // Envoi de la demande d'écriture au serveur
     int sendSize = sendto(sck, buffer, size, 0, res->ai_addr, res->ai_addrlen);
     if (sendSize == -1) {
         perror("sendto");
@@ -60,23 +65,27 @@ int main(int argc, char *argv[]) {
     unsigned short blockCounter = 0;
 
     while (1) {
+        // Construction du bloc de données à envoyer
         unsigned char buffer[MAX_BUFFER_SIZE];
         buffer[0] = 0;
         buffer[1] = 3; // Opcode 3 pour les données
         buffer[2] = (blockCounter >> 8) & 0xFF;
         buffer[3] = blockCounter & 0xFF;
 
+        // Lecture du fichier
         fseek(fileOut, blockCounter * DATA_BLOCK_SIZE, SEEK_SET);
         size_t bytesRead = fread(&buffer[4], 1, DATA_BLOCK_SIZE, fileOut);
 
         int dataSize = bytesRead + 4;
 
+        // Envoi du bloc de données au serveur
         int sendSize = sendto(sck, buffer, dataSize, 0, res->ai_addr, res->ai_addrlen);
         if (sendSize == -1) {
             perror("sendto data");
             return 1;
         }
 
+        // Réception de l'ACK du serveur
         struct sockaddr_storage server_addr;
         socklen_t addrsize = sizeof(server_addr);
         int recvSize = recvfrom(sck, buffer, MAX_BUFFER_SIZE, 0, (struct sockaddr *)&server_addr, &addrsize);
@@ -85,6 +94,7 @@ int main(int argc, char *argv[]) {
             return 1;
         }
 
+        // Vérification de l'ACK reçu
         unsigned short blockId = (buffer[2] << 8) | buffer[3];
 
         if (blockId == blockCounter) {
@@ -94,6 +104,7 @@ int main(int argc, char *argv[]) {
             continue;
         }
 
+        // Vérification de la fin du transfert de fichier
         if (bytesRead < DATA_BLOCK_SIZE) {
             printf("File transfer completed.\n");
             break;
@@ -102,10 +113,10 @@ int main(int argc, char *argv[]) {
         blockCounter++;
     }
 
+    // Fermeture du fichier et libération des ressources
     fclose(fileOut);
     freeaddrinfo(res);
     close(sck);
     return 0;
 }
-
 
